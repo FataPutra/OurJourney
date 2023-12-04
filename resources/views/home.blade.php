4fbc4225b600b0860bb3150438271933bf3a00a4
Fata Putra <111202113905@mhs.dinus.ac.id>
2023-12-04 13:36:40 +0700

    First Commit

---
+++ resources/views/home.blade.php
@@ -0,0 +1,15 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Home</title>
+</head>
+<body>
+    <h1>INI ADALAH HALAMAN HOME</h1>
+    <H2>
+        {{ $name }}
+    </H2>
+</body>
+</html>
